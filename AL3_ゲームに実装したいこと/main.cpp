728c781e35af1dcd12f37dad3afd361b95ba7f81
sora <147121795+sora7716@users.noreply.github.com>
2024-08-23 12:49:22 +0900

    bind関数で束縛した

--- AL3_ゲームに実装したいこと/main.cpp
+++ AL3_ゲームに実装したいこと/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <functional>
 using namespace std;
 
-int main() {
+//加算
+int Add(int a, int b, int c) {
+	return a + b + c;
+}
 
-	cout << "ああああ" << endl;
+int main() {
+	//第1引数のみを先に渡す
+	function<int(int,int)>f = bind(Add, 2, placeholders::_1, placeholders::_2);
+	//残りの引数を渡す
+	const int result = f(3, 4);
+	cout << result << endl;
 	return 0;
 }
