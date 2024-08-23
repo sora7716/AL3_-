9d2995159c3c9cde20a0e17745f60cc703676de7
sora <147121795+sora7716@users.noreply.github.com>
2024-08-23 12:34:35 +0900

    std::fuctionを使ってAdd関数とラムダ式を代入した

--- AL3_ゲームに実装したいこと/main.cpp
+++ AL3_ゲームに実装したいこと/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <functional>
 using namespace std;
+
+//加算
+int Add(int x) { return x + 1; }
 
 int main() {
 
-	cout << "ああああ" << endl;
+	/*どちらでも大丈夫↓*/
+	//関数の代入
+	function<int(int)>f = Add;
+	//ラムダ式を代入
+	f = [](int x) {return x + 1; };
+	/*どちらでも大丈夫↑*/
+
+	//保持している関数を呼び出す
+	int result = f(1);
+	cout << result << endl;
 	return 0;
 }
